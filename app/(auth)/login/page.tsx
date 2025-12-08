a3fe1143b82a9b63f868978f6118c2a7ee61c037
wajahat taj <p233023@pwr.nu.edu.pk>
2025-12-08 20:27:57 +0500

    fixed last

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,6 +1,10 @@
-"use client";
 
-import { Suspense, useEffect, useState } from "react";
+
+"use client";
+export const dynamic = "force-dynamic";
+
+
+import { useEffect, useState } from "react";
 import Link from "next/link";
 import { useRouter, useSearchParams } from "next/navigation";
 import { Lock, Mail, Scale } from "lucide-react";
